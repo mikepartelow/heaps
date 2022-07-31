189d0e82ea75a0c4e2a12acac44d98181bdb56ca
Mike Partelow <github@mikepartelow.com>
2022-07-30 17:01:01 -0700

    generic heap and benchmarks

--- heaps.go
+++ heaps.go
@@ -1,25 +1,53 @@
 package heaps
 
-type Thing struct {
-	Name  string
-	Value int
+type Heap[T any] struct {
+	items []T
+	cmp   func(a, b T) int
 }
 
-type Things []*Thing
-
-// sort.Interface
-func (t *Things) Len() int           { return len(*t) }
-func (t *Things) Less(i, j int) bool { return (*t)[i].Value < (*t)[j].Value }
-func (t *Things) Swap(i, j int)      { (*t)[i], (*t)[j] = (*t)[j], (*t)[i] }
-
-// heap.Interface
-func (t *Things) Push(x any) {
-	*t = append(*t, x.(*Thing))
+func NewHeap[T any](items []T, cmp func(a, b T) int) *Heap[T] {
+	h := Heap[T]{items: items, cmp: cmp}
+	min_heapify(h.items, 0, h.cmp)
+	return &h
 }
 
-func (t *Things) Pop() any {
-	n := len(*t)
-	thing := (*t)[n-1]
-	*t = (*t)[0 : n-1]
-	return thing
+func (h *Heap[T]) Push(item T) {
+	parent := func(i int) int {
+		return (i - 1) / 2
+	}
+
+	h.items = append(h.items, item)
+
+	for i := len(h.items) - 1; h.cmp(h.items[parent(i)], h.items[i]) == 1; i = parent(i) {
+		h.items[i], h.items[parent(i)] = h.items[parent(i)], h.items[i]
+	}
+}
+
+func (h *Heap[T]) Pop() T {
+	x := h.items[0]
+
+	h.items[0] = h.items[len(h.items)-1]
+	h.items = h.items[:len(h.items)-1]
+	min_heapify(h.items, 0, h.cmp)
+
+	return x
+}
+
+func min_heapify[T any](arr []T, idx int, cmp func(a, b T) int) {
+	left := 2*idx + 1
+	right := 2*idx + 2
+	smallest := idx
+
+	if left < len(arr) && cmp(arr[left], arr[smallest]) == -1 {
+		smallest = left
+	}
+
+	if right < len(arr) && cmp(arr[right], arr[smallest]) == -1 {
+		smallest = right
+	}
+
+	if smallest != idx {
+		arr[idx], arr[smallest] = arr[smallest], arr[idx]
+		min_heapify(arr, smallest, cmp)
+	}
 }
--- heaps_test.go
+++ heaps_test.go
@@ -5,18 +5,44 @@ import (
 	"mp/heaps"
 	"sort"
 	"testing"
+
+	"golang.org/x/exp/constraints"
 )
 
-func TestSort(t *testing.T) {
-	got := heaps.Things{
-		&heaps.Thing{"thing 3", 3},
-		&heaps.Thing{"thing 1", 1},
-		&heaps.Thing{"thing 2", 2},
+type Thing struct {
+	Name  string
+	Value int
+}
+
+type Things []*Thing
+
+// sort.Interface
+func (t *Things) Len() int           { return len(*t) }
+func (t *Things) Less(i, j int) bool { return (*t)[i].Value < (*t)[j].Value }
+func (t *Things) Swap(i, j int)      { (*t)[i], (*t)[j] = (*t)[j], (*t)[i] }
+
+// heap.Interface
+func (t *Things) Push(x any) {
+	*t = append(*t, x.(*Thing))
+}
+
+func (t *Things) Pop() any {
+	n := len(*t)
+	thing := (*t)[n-1]
+	*t = (*t)[0 : n-1]
+	return thing
+}
+
+func TestThingsSort(t *testing.T) {
+	got := Things{
+		&Thing{"thing 3", 3},
+		&Thing{"thing 1", 1},
+		&Thing{"thing 2", 2},
 	}
-	want := heaps.Things{
-		&heaps.Thing{"thing 1", 1},
-		&heaps.Thing{"thing 2", 2},
-		&heaps.Thing{"thing 3", 3},
+	want := Things{
+		&Thing{"thing 1", 1},
+		&Thing{"thing 2", 2},
+		&Thing{"thing 3", 3},
 	}
 	sort.Sort(&got)
 
@@ -27,27 +53,121 @@ func TestSort(t *testing.T) {
 	}
 }
 
-func TestHeap(t *testing.T) {
-	pq := heaps.Things{
-		&heaps.Thing{"thing 3", 3},
-		&heaps.Thing{"thing 1", 1},
-		&heaps.Thing{"thing 2", 2},
+func cmp[T constraints.Ordered](a, b T) int {
+	if a < b {
+		return -1
+	}
+	if a > b {
+		return 1
+	}
+	return 0
+}
+
+func cmpThings(a, b Thing) int {
+	return cmp(a.Value, b.Value)
+}
+
+func cmpThingPtrs(a, b *Thing) int {
+	return cmp(a.Value, b.Value)
+}
+
+func TestThingsHeap(t *testing.T) {
+	pq := Things{
+		&Thing{"thing 3", 3},
+		&Thing{"thing 1", 1},
+		&Thing{"thing 2", 2},
 	}
 
 	heap.Init(&pq)
-	assertThing(t, heap.Pop(&pq), 1)
-	assertThing(t, heap.Pop(&pq), 2)
+	assertAnyThing(t, heap.Pop(&pq), 1)
+	assertAnyThing(t, heap.Pop(&pq), 2)
 
-	heap.Push(&pq, &heaps.Thing{"thing 1", 1})
+	heap.Push(&pq, &Thing{"thing 1", 1})
 
-	assertThing(t, heap.Pop(&pq), 1)
-	assertThing(t, heap.Pop(&pq), 3)
+	assertAnyThing(t, heap.Pop(&pq), 1)
+	assertAnyThing(t, heap.Pop(&pq), 3)
 }
 
-func assertThing(t testing.TB, thing any, want int) {
+func BenchmarkThingsHeap(b *testing.B) {
+	pq := Things{}
+	heap.Init(&pq) // just for giggles
+	for i := 0; i < b.N; i++ {
+		heap.Push(&pq, &Thing{"something", i})
+	}
+	for i := 0; i < b.N; i++ {
+		j := heap.Pop(&pq).(*Thing)
+		if j.Value != i {
+			b.Errorf("expected %d, got %d", i, j.Value)
+		}
+	}
+}
+
+func TestGenericHeap(t *testing.T) {
+	t.Run("ints", func(t *testing.T) {
+		pq := heaps.NewHeap([]int{3, 2, 1}, cmp[int])
+
+		assertEqual(t, pq.Pop(), 1)
+		assertEqual(t, pq.Pop(), 2)
+
+		pq.Push(1)
+
+		assertEqual(t, pq.Pop(), 1)
+		assertEqual(t, pq.Pop(), 3)
+	})
+
+	t.Run("float64s", func(t *testing.T) {
+		pq := heaps.NewHeap([]float64{3.3, 2.2, 1.1}, cmp[float64])
+
+		assertEqual(t, pq.Pop(), 1.1)
+		assertEqual(t, pq.Pop(), 2.2)
+
+		pq.Push(1.1)
+
+		assertEqual(t, pq.Pop(), 1.1)
+		assertEqual(t, pq.Pop(), 3.3)
+	})
+
+	t.Run("things", func(t *testing.T) {
+		pq := heaps.NewHeap([]Thing{
+			Thing{"thing 3", 3},
+			Thing{"thing 2", 2},
+			Thing{"thing 1", 1},
+		}, cmpThings)
+
+		assertEqual(t, pq.Pop(), Thing{"thing 1", 1})
+		assertEqual(t, pq.Pop(), Thing{"thing 2", 2})
+
+		pq.Push(Thing{"thing 1", 1})
+
+		assertEqual(t, pq.Pop(), Thing{"thing 1", 1})
+		assertEqual(t, pq.Pop(), Thing{"thing 3", 3})
+	})
+}
+
+func BenchmarkGenericHeapOfThingPtrs(b *testing.B) {
+	pq := heaps.NewHeap([]*Thing{}, cmpThingPtrs)
+	for i := 0; i < b.N; i++ {
+		pq.Push(&Thing{"something", i})
+	}
+	for i := 0; i < b.N; i++ {
+		j := pq.Pop()
+		if j.Value != i {
+			b.Errorf("expected %d, got %d", i, j.Value)
+		}
+	}
+}
+
+func assertAnyThing(t testing.TB, thing any, want int) {
 	t.Helper()
-	got := thing.(*heaps.Thing).Value
+	got := thing.(*Thing).Value
 	if got != want {
 		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func assertEqual[T comparable](t testing.TB, got, want T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %v, wanted %v", got, want)
 	}
 }
